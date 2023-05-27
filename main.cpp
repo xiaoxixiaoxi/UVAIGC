205b950186f6c5d0042025c431d6a8a2771db2b1
linximing <654222770@qq.com>
2023-05-27 08:58:45 +0800

    win修改

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "user/UserModule/UserModule.hpp"
 #include "user/UserMainWindow.h"
 #include <QMenuBar>
-//添加一个注释纯纯粹粹
+//添加一个注释纯纯粹粹 win修改
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
